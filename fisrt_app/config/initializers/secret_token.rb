6f43c35b142bfdbd100c42df9cc71e06dd239a67
yutaro24 <yutaro.toyoda@gmail.com>
2015-07-26 16:53:44 +0900

    Initialize repository

---
+++ fisrt_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FisrtApp::Application.config.secret_key_base = 'db9cd88c22382999b4fa7b3ca83c51b1f85472dc3d840db31bfaa6114c0f8865aa181c3c2de25cca3c9a637e8ee1ba380744196935f735276b9c13ef5da14392'
---
+++ fisrt_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FisrtApp::Application.config.session_store :cookie_store, key: '_fisrt_app_session'
